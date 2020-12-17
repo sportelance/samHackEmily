f5f7259ff4a2008e440d34203ad0de66da08cd5b
Sam Portelance <sportelance1@gmail.com>
2020-12-16 21:21:26 -0500

    f:

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const contents = document.getElementById('contents');
 contents.parentNode.removeChild(contents);
+
+//InconsPic
+    //get every header   
+    //check their inner text
+    //if 
+    // 
+    // 
+
